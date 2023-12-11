50595b56f505e4ae732c8a58462fcd090898c2b5
Arup Bhowmik <aruprb14@gmail.com>
2023-12-10 21:14:24 -0500

    Blurry loading

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,6 +12,30 @@ import { AuthProvider } from "./auth";
 import { initializeApp } from "firebase/app";
 import firebaseConfig from "./firebase.config";
 
+// arup
+
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
+
+let load = 0;
+let int = setInterval(blurring, 30);
+
+function blurring() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(int);
+  }
+  loadText.innerHTML = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_max) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
+// arup
+
 initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
