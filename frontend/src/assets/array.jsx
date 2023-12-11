db4c7d6715c40d9185aebcc00a552eac47b22317
AhnafHamim <hamimnycusa@gmail.com>
2023-12-10 23:15:19 -0500

    Added tips pictures

---
+++ frontend/src/assets/array.jsx
@@ -0,0 +1,6 @@
+const tipsData = [
+    { img: 'Screenshot 2023-12-10 220131.png', title: 'Tip 1' },
+    { img: 'Screenshot 2023-12-10 220215.png', title: 'Tip 2' },
+    { img: 'Screenshot 2023-12-10 220339.png', title: 'Tip 3' },
+    { img: 'Screenshot 2023-12-10 22022125.png', title: 'Tip 4' }
+  ];
